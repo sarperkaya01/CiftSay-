95638be198e3e7fe14f690674cf87f1dccfbc7f9
sarperkaya01 <sarperkaya01@gmail.com>
2021-11-03 19:32:44 +0300

    commitTersten

---
+++ TerstenYazdırma/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TerstenYazdırma
+{
+    class Program
+    {
+        static void deger(int n)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (n % x == 0 || n == x)
+            {
+                Console.WriteLine(x + " " + n + " sayısının tam bölenidir ");
+                Console.WriteLine(n + " mod " + x + " = " + (n % x));
+            }
+            else
+            {
+                Console.WriteLine(x + " " + n + " sayısının tam böleni değildir ");
+                Console.WriteLine(n + " mod " + x + " = " + (n % x));
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+
+            while (kontrol)
+            {
+                Console.Write("Sınamak istediğiniz sayı belirleyiniz :");//m
+                int m = int.Parse(Console.ReadLine());
+                Console.Write("Kaç kez Sınamak istediğinizi giriniz :");//n
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a > 0 && m > 0)
+                {
+                    for (int i = 1; i <= a; i++)
+                    {
+                        Console.Write(i + ". " + "Sayıyı giriniz: ");
+
+                        deger(m);
+
+                    }
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Girdiğiniz sayı/sayılar negatif...");
+                }
+                Console.WriteLine("Tekrar işlem yapmak için A Tuşuna basın ya da çıkmak için Q tuşuna basın...");
+
+                string q = Console.ReadLine();
+                if (q == "a")
+                {
+                    kontrol = true;
+                }
+                else if (q == "q")
+                {
+                    kontrol = false;
+                }
+            }
+        }
+    }
+}
