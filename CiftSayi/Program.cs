58dc66a7c05abfaf8e79365bc4ca6b2513ba7f72
sarperkaya01 <sarperkaya01@gmail.com>
2021-11-03 19:45:58 +0300

    commitCift2

--- CiftSayi/Program.cs
+++ CiftSayi/Program.cs
@@ -18,6 +18,7 @@ namespace CiftSayi
         static void Main(string[] args)
         {
             bool kontrol = true;
+            bool kontrol2 = true;
 
             while (kontrol)
             {
@@ -39,17 +40,29 @@ namespace CiftSayi
                 {
                     Console.WriteLine("Girdiğiniz sayı negatif...");
                 }
-                Console.WriteLine("Tekrar işlem yapmak için A Tuşuna basın ya da çıkmak için Q tuşuna basın...");
+                while(kontrol2){Console.WriteLine("Tekrar işlem yapmak için A Tuşuna basın ya da çıkmak için Q tuşuna basın...");
 
                 string q = Console.ReadLine();
                 if (q == "a")
                 {
                     kontrol = true;
+                    kontrol2 = false;
+
+
                 }
                 else if (q == "q")
                 {
                     kontrol = false;
+                    kontrol2= false;
                 }
+                else{
+                    Console.WriteLine( "Yanlış bir tuşlama yaptınız"  );
+                    kontrol2=false;
+                }
+                
+
+                }
+                
             }
         }
     }
